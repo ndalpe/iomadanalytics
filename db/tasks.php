7e3c9ff45d8fb1b838159f2b1584e882656f05d0
ndalpe <ndalpe@gmail.com>
2018-01-25 15:31:58 +0700

    Added Task to render the Filter Tab System

--- db/tasks.php
+++ db/tasks.php
@@ -18,5 +18,14 @@ $tasks = array(
 		'day' => '*',
 		'dayofweek' => '*',
 		'month' => '*'
+	),
+	array(
+		'classname' => 'report_iomadanalytics\task\FilterTabs',
+		'blocking' => 0,
+		'minute' => '0',
+		'hour' => '3',
+		'day' => '*',
+		'dayofweek' => '1-5',
+		'month' => '*'
 	)
 );
