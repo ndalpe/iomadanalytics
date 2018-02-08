fdd638d11a143c20b3c9c0474e1f77dbe61a05c8
ndalpe <ndalpe@gmail.com>
2018-02-08 23:15:05 +0700

    Freeze country selector control while AJAX call is being made

--- amd/src/iomadanalytics.js
+++ amd/src/iomadanalytics.js
@@ -1,10 +1,10 @@
 define(
     [
         'jquery',
-        '/report/iomadanalytics/amd/src/Chart.min.js',
-        'core/ajax'
+        'core/ajax',
+        '/report/iomadanalytics/amd/src/Chart.min.js'
     ],
-    function($, Chart, ajax) {
+    function($, ajax, Chart) {
     return {
         init: function() {
             $(document).ready(function() {
@@ -20,28 +20,6 @@ define(
                 $.getJSON(pluginPath+"graph_grades_all_companies.json", function(gData){
                     makeGradesGraph(gData);
                 });
-
-                function makeGradesGraph(gData) {
-
-                    if (gradesGraph!==null) {
-                        gradesGraph.destroy();
-                    }
-
-                    gradesGraph = new Chart(document.getElementById("chart-grades").getContext("2d"),
-                        {type:'bar', data:gData, options: {
-                            scales: {yAxes: [{ticks: {beginAtZero:true}}]},
-                            tooltips: {
-                                enabled: true,
-                                mode: 'single',
-                                callbacks: {
-                                    label: function(tooltip, data) {
-                                        return data.datasets[tooltip.datasetIndex].label + ' : ' + tooltip.yLabel + '%';
-                                    }
-                                }
-                            }
-                        }}
-                    );
-                } //makeGradesGraph()
 
                 // Make the default progress graph of all companies
                 // $.getJSON(pluginPath+"graph_progress_all_companies.json", function(gData){
@@ -76,28 +54,18 @@ define(
                     toggleChekbox(countries[i]);
                 }
 
-                // check/uncheck all company under a country when the country checkbox is clicked
-                function toggleChekbox(country) {
-                    // check/uncheck all checkbox of the country
-                    $("#"+country).click(function() {
-                        if ($("#"+country).prop('checked') === true) {
-                            $('.country-'+country+' input').prop('checked', true);
-                        } else {
-                            $('.country-'+country+' input').prop('checked', false);
-                        }
-                        // refresh grades graph with new country selection
-                        refreshGradesGraph();
-                    });
-                } // end toggleChekbox()
-
-                // $("#btn").click(function(){refreshGradesGraph();});
-
                 // refresh the grades graph when a company checkbox is checked/unchecked
                 $(".country_company input").change(function(){
                     refreshGradesGraph();
                 });
 
+                /**
+                 * Ajax a new dataset according active country and filter
+                */
                 function refreshGradesGraph() {
+
+                    // Freeze all checkbox and button while the graph is being refreshed
+                    freezeControl(true);
 
                     // Get selected companies
                     var companies = [];
@@ -114,17 +82,101 @@ define(
                     // build param object
                     var params = JSON.stringify({companies:companies, filters:filters});
 
-                    var promises = ajax.call([
-                        {methodname: 'report_iomadanalytics_filters', args: {filters: params}}
-                    ]);
+                    var promises = ajax.call(
+                        [{methodname:'report_iomadanalytics_filters', args:{filters:params}}]
+                    );
 
                     promises[0]
                     .done(function(response) {
+                        // Make the graph with newly received data
                         makeGradesGraph($.parseJSON(response));
+
+                        // unfreeze controls
+                        freezeControl(false);
                     })
                     .fail(function(response) {
                         console.log('fail');
                         console.log(response);
+                    });
+                }
+
+                /**
+                 * Creates the grade bar chart according to new dataset passed in attributes
+                 * param: gradesGraphData JSON - The new graph's dataset
+                */
+                function makeGradesGraph(gradesGraphData) {
+                    // if the graph object exists, destroy it
+                    // to avoid hover glitch with previous datasets
+                    if (gradesGraph!==null) {
+                        gradesGraph.destroy();
+                    }
+
+                    // create the graph
+                    gradesGraph = new Chart(document.getElementById("chart-grades").getContext("2d"),
+                        {type:'bar', data:gradesGraphData, options: {
+                            scales: {yAxes: [{ticks: {beginAtZero:true}}]},
+                            tooltips: {
+                                enabled: true,
+                                mode: 'single',
+                                callbacks: {
+                                    label: function(tooltip, data) {
+                                        return data.datasets[tooltip.datasetIndex].label + ' : ' + tooltip.yLabel + '%';
+                                    }
+                                }
+                            }
+                        }}
+                    );
+                }
+
+                /**
+                 * Check/uncheck all company under a country
+                 * param: country string - The country's 2 letters abbr (ie.: ID or MY or ...)
+                */
+                function toggleChekbox(country) {
+                    // check/uncheck all checkbox of the country
+                    $("#"+country).click(function() {
+                        if ($("#"+country).prop('checked') === true) {
+                            $('.country-'+country+' input').prop('checked', true);
+                        } else {
+                            $('.country-'+country+' input').prop('checked', false);
+                        }
+                        // refresh grades graph with new country selection
+                        refreshGradesGraph();
+                    });
+                } // end toggleChekbox()
+
+                /**
+                 * Disable the country selector and filters button
+                 * param: state bool - whether the input should be disable or not
+                */
+                function freezeControl(state) {
+                    // freeze countries checkbox
+                    $(".country_company input, .country input, #filters_tab button").each(function(){
+                        $(this).attr('disabled', state);
+                    });
+                }
+
+                /**
+                 * Set the selected filter in hidden element
+                */
+                $('#filters_tab button').click(function(){
+                    $("#selectedFilter").val($(this).data('filterid'));
+                    resetFilterBtnStyle();
+                    $(this).html('<i class="fa fa-check"></i> '+$(this).html()).addClass('bkgBtn');
+
+                    // refresh the grades graph with the new filter
+                    refreshGradesGraph();
+                });
+
+                /**
+                 * Remove the check mark and bkg color on the selected filter button
+                */
+                function resetFilterBtnStyle() {
+                    $('#filters_tab button').each(function(){
+                        $(this).html(
+                            $("<div>").html($(this).html()).text()
+                        )
+                        .removeClass('bkgBtn');
                     });
                 }
 
@@ -137,26 +189,6 @@ define(
                     $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
                     $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
                 });
-
-                // set the selected filter in hidden element
-                $('#filters_tab button').click(function(){
-                    $("#selectedFilter").val($(this).data('filterid'));
-                    resetFilterBtnStyle();
-                    $(this).html('<i class="fa fa-check"></i> '+$(this).html()).addClass('bkgBtn');
-
-                    // refresh the grades graph with the new filter
-                    refreshGradesGraph();
-                });
-
-                // remove the check mark and bkg color on the selected filter button
-                function resetFilterBtnStyle() {
-                    $('#filters_tab button').each(function(){
-                        $(this).html(
-                            $("<div>").html($(this).html()).text()
-                        )
-                        .removeClass('bkgBtn');
-                    });
-                }
 
             }); // end document.ready
         } // end: init
