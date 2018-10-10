f1ffaec79270371ba064010c2c7a6d5553f28dd8
ndalpe <ndalpe@gmail.com>
2018-10-10 15:02:33 +0700

    Added file path in FlatFile logging class

--- classes/FlatFile.php
+++ classes/FlatFile.php
@@ -2,15 +2,39 @@
 
 /**
 * Write data to a flat file, mainly for caching purpose
+
+include('/var/www/html/report/iomadanalytics/classes/FlatFile.php');
+$f = new FlatFile();
+$f->setFilePath($CFG->dirroot.'/theme/kpdesktop/');
+$f->setFileName('theme.txt');
+$f->setObContent($PAGE);
+$f->writeToFile();
+
 */
 class FlatFile
 {
+	// The path that the file should be created in
+	public $path;
 
 	// The file name to write in
 	public $file;
 
 	// The content to write in $this->file
-	public $content;
+	public $content = '';
+
+	public function setFilePath($path)
+	{
+		$this->path = $path;
+	}
+
+	public function getFilePath()
+	{
+		if (empty($this->path)) {
+			return $CFG->dirroot.'/report/iomadanalytics/templates/';
+		} else {
+			return $this->path;
+		}
+	}
 
 	public function setFileName($file)
 	{
@@ -39,14 +63,15 @@ class FlatFile
 	{
 		ob_start();
 		var_dump($ob);
-		$this->content = ob_get_contents();
+		$this->content .= ob_get_contents();
 		ob_end_clean();
 	}
 
 	public function writeToFile()
 	{
 		global $CFG;
-		$myfile = fopen($CFG->dirroot."/report/iomadanalytics/templates/{$this->file}", "w+") or die("Unable to open file!");
+		$path = $this->getFilePath();
+		$myfile = fopen($path.$this->file, "r+");
 		fwrite($myfile, $this->content);
 		fclose($myfile);
 	}
